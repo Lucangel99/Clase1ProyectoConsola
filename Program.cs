4232a1aa4263c24afe83fca5e3bb1ebf87400bb6
Lucangel99 <44577277+Lucangel99@users.noreply.github.com>
2021-12-08 12:06:24 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("chauchas");
+
+
+var user = new User("kike","keki");
+
+
+Console.WriteLine(user.FullName());
+
+
+
